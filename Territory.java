77a3c04de974cc12780b67bd4d7b90785aa95475
Gabriele Herr <62764570+gabrieleherr@users.noreply.github.com>
2022-01-09 09:46:07 -0500

    upload territory.java
    territory fields:
    - neighbors (array of territories)
    - type (enum SEA, NEUTRAL, OWNED)
    - name
    - isCapital?
    - points its worth
    - owner (player)
    - occupying troops (arraylist of units)

--- Territory.java
+++ Territory.java
@@ -1,10 +1,47 @@
+import java.util.ArrayList;
 
 public class Territory {
 
-	private enum type {SEA, LAND}
+	public enum territoryType {SEA, NEUTRAL, OWNED}
 	
-	public Territory(String init_type) {
-		
+	private Territory[] neighbors;
+	private territoryType type;
+	private String name;
+	private boolean isCapital;
+	private int points;
+	private Player owner;
+	private ArrayList<Unit> occupyingTroops;
+	
+	// owned by a player
+	public Territory(Territory[] ineighbors, String iname, boolean iscapital, int ipoints, 
+			Player p, ArrayList<Unit> troops) {
+		type = territoryType.OWNED;
+		neighbors = ineighbors;
+		name = iname;
+		isCapital = iscapital;
+		points = ipoints;
+		owner = p;
+		occupyingTroops = troops;
+	}
+	
+	// neutral territory
+	public Territory(Territory[] ineighbors, String iname) {
+		type = territoryType.NEUTRAL;
+		neighbors = ineighbors;
+		name = iname;
+		occupyingTroops = new ArrayList<Unit>();
+	}
+	
+	// sea territory
+	public Territory(Territory[] ineighbors, ArrayList<Unit> troops) {
+		type = territoryType.SEA;
+		neighbors = ineighbors;
+		occupyingTroops = troops;
+		name = "";
+	}
+	
+	public int getPoints() {
+		return points;
 	}
 	
 }
